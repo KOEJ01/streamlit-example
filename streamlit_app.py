ad6f79c6ff98d025b8b3b095527cb6f25734def4
KOEJ01 <129171388+KOEJ01@users.noreply.github.com>
2023-03-28 13:16:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,23 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
+import yfinance as yf
+import datetime as dt
 
-"""
-# Welcome to Streamlit!
+# Set the stock symbol and the strike price
+symbol = 'AAPL'
+strike_price = 150
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Set the start and end dates
+start_date = dt.datetime.today()
+end_date = start_date + dt.timedelta(days=365)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Get the options data for the specified stock and strike price
+options = yf.Ticker(symbol).option_chain(date=start_date.strftime('%Y-%m-%d'))
+calls = options.calls[options.calls['strike']==strike_price]
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Loop through each expiration date in the next 12 months and get the options data
+for i in range(12):
+    expiration_date = start_date.replace(day=1) + dt.timedelta(days=31) - dt.timedelta(days=1)
+    options = yf.Ticker(symbol).option_chain(date=expiration_date.strftime('%Y-%m-%d'))
+    calls = options.calls[options.calls['strike']==strike_price]
+    print('Options data for expiration date:', expiration_date.strftime('%Y-%m-%d'))
+    print(calls)
+    start_date = expiration_date + dt.timedelta(days=1)
